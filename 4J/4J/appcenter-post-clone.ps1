91173b01b8808db59ad727f3475237b5d49497f6
Haim B <haim.burla@gmail.com>
2020-11-28 21:41:31 +0200

    Trying to set app center secret on the build machine.

---
+++ 4J/4J/appcenter-post-clone.ps1
@@ -0,0 +1,25 @@
+﻿#!/usr/bin/env bash
+#
+#
+# DECLARE THE APP_CENTER_SECRETS_JSON ENVIRONMENT VARIABLE IN APP CENTER BUILD CONFIGURATION, SET
+# TO THE CONTENTS OF YOUR secrets.json FILE
+
+if [ -z "$APP_CENTER_SECRETS_JSON" ]
+then
+    echo "You need define the APP_CENTER_SECRETS_JSON variable in App Center"
+    exit
+fi
+
+# This is the path to the secrets.json file, Update '4J\4J\4J' to be the
+# correct path to the file relative to the root of your repo
+APP_CENTER_SECRETS_JSON_FILE=$APPCENTER_SOURCE_DIRECTORY/4J/4J/secrets.json
+
+if [ -e "$APP_CENTER_SECRETS_JSON_FILE" ]
+then
+    echo "Updating secrets.json"
+    echo "$APP_CENTER_SECRETS_JSON" > $APP_CENTER_SECRETS_JSON_FILE
+    sed -i -e 's/\\"/'\"'/g' $APP_CENTER_SECRETS_JSON_FILE
+
+    echo "File content:"
+    cat $APP_CENTER_SECRETS_JSON_FILE
+fi
